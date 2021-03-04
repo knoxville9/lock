2af2cd394eb255cb3f65cfe7c1190c869e52ced7
knoxville <knoxville.lee9@gmail.com>
2021-03-04 17:23:35 +0800

    golang 学习

---
+++ fakeSearch/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/petermattis/goid"
+	uuid "github.com/satori/go.uuid"
+	"math/rand"
+	"runtime"
+	"time"
+)
+
+type Result struct {
+	video string
+	id    int64
+}
+
+func fakeSeach() <-chan Result {
+	c := make(chan Result)
+	go func() {
+		for {
+			rand.Seed(time.Now().Unix())
+			time.Sleep(time.Duration(rand.Intn(1e2)) * time.Millisecond)
+			result := Result{
+				video: uuid.NewV4().String(),
+				id:    goid.Get(),
+			}
+			c <- result
+
+		}
+	}()
+	return c
+}
+
+func main() {
+	i := []<-chan Result{fakeSeach(), fakeSeach(), fakeSeach()}
+
+	fanIn(i...)
+	fmt.Println(runtime.NumGoroutine())
+
+}
+func fanIn(a ...<-chan Result) {
+
+	after := time.After(50 * time.Millisecond)
+	for _, results := range a {
+		select {
+		case a := <-results:
+			fmt.Println(a)
+			return
+		case <-after:
+			fmt.Println("wait 50 ms,try one more time")
+			return
+
+		}
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module lock
+
+go 1.15
+
+require (
+	github.com/go-delve/delve v1.6.0 // indirect
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.10 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5
+	github.com/satori/go.uuid v1.2.0
+)
